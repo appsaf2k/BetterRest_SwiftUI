8210ac7c9d266b277a7a34545766c7ffeaffbb34
appsaf2k <appsaf2k@gmail.com>
2022-08-31 10:27:53 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by @andreev2k on 31.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
